5807b51bb787a2b38666be0c4cb83896e12e7348
nobody <nobody@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2014-12-05 22:35:13 +0000

    Start commit.

---
+++ trunk/IDataField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeaFightGame
+{
+    public interface IDataField
+    {
+        Cell GetCell(int i, int j);
+        IEnumerable<Cell> GetCells();
+        IEnumerable<Ship> GetShips();
+    }
+}
---
+++ trunk/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeaFightGame
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataField df = new DataField();
+            df.AddShip(new Ship { X1 = 0, X2 = 2, Y1 = 0, Y2 = 0 });
+            df.AddShip(new Ship { X1 = 5, X2 = 5, Y1 = 5, Y2 = 7 });
+            player1Field.DataField = df;
+            player2Field.DataField = new DataField();
+        }
+    }
+}
