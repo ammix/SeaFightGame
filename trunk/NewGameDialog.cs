31bbd3f14549a36e14b7fc1dae6dda9f1c35fe72
nobody <nobody@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2014-12-16 23:44:54 +0000

    Remove BindWithShips.

--- trunk/NewGameDialog.cs
+++ trunk/NewGameDialog.cs
@@ -16,11 +16,11 @@ namespace SeaFightGame
             InitializeComponent();
         }
 
-        public bool CpuShipSetup { get; set; }
+        public bool AiShipSetup { get; set; }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            CpuShipSetup = checkBox1.Checked;
+            AiShipSetup = checkBox1.Checked;
         }
 
         private void okButton_Click(object sender, EventArgs e)
